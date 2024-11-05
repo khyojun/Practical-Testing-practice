bbed31a4f1a1fb068dcec1b93b73ccaf622ea666
Khyojun <nandong5923@naver.com>
2024-11-05 16:11:21 +0900

    feat #5 테스트 어려운 영역 분리하여 깨지지 않는 테스트 만들기

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -4,18 +4,30 @@ import lombok.Getter;
 import sample.cafekiosk.unit.berverage.Berverage;
 import sample.cafekiosk.unit.order.Order;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class CafeKiosk {
 
+
+    public static final LocalTime SHOP_OPEN_TIME = LocalTime.of(10, 0);
+    public static final LocalTime SHOP_CLOSE_TIME = LocalTime.of(22, 0);
+
+
     private final List<Berverage> berverages = new ArrayList<>();
+
+
+    public void add(Berverage berverage) {
+        berverages.add(berverage);
+    }
 
     public void add(Berverage berverage, int count) {
 
-        if(count <= 0){
+        if (count <= 0) {
             throw new IllegalArgumentException("음료의 수량은 0보다 작을 수 없습니다.");
         }
 
@@ -43,6 +55,24 @@ public class CafeKiosk {
 
 
     public Order createOrder() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalTime currentTime = now.toLocalTime();
+
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("현재 주문이 가능한 시간이 아닙니다.");
+        }
+
+        return new Order(LocalDateTime.now(), berverages);
+    }
+
+
+    public Order createOrder(LocalDateTime now) {
+        LocalTime currentTime = now.toLocalTime();
+
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("현재 주문이 가능한 시간이 아닙니다.");
+        }
+
         return new Order(LocalDateTime.now(), berverages);
     }
 
--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -3,6 +3,10 @@ package sample.cafekiosk.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.berverage.Americano;
 import sample.cafekiosk.unit.berverage.Latte;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -86,6 +90,49 @@ class CafeKioskTest {
 
         cafeKiosk.clear();
         assertThat(cafeKiosk.getBerverages().size()).isEqualTo(0);
+    }
+
+
+
+    @Test
+    void createOrder(){ // 이 친구는 항상 성공하는 테스트가 아님! 시간 따라 결과가 달라짐!
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        Order order = cafeKiosk.createOrder();
+
+        assertThat(order.getBerverages()).hasSize(1);
+        assertThat(order.getBerverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+
+    @Test
+    void createOrderWithCurrentTime(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+        Order order = cafeKiosk.createOrder(LocalDateTime.of(2024,11,5,10,0));
+
+        assertThat(order.getBerverages()).hasSize(1);
+        assertThat(order.getBerverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+
+    @Test
+    void createOrderOutsideOpenTime(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        Americano americano = new Americano();
+        cafeKiosk.add(americano);
+
+
+        assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2024,11,5,9,59)))
+                .isInstanceOf((IllegalArgumentException.class))
+                .hasMessage("현재 주문이 가능한 시간이 아닙니다.");
     }
 
 
