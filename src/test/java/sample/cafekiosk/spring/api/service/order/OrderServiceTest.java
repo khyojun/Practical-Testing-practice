0e01f0567dd089b358f18bba9a8b581b4d810d3f
Khyojun <nandong5923@naver.com>
2024-11-18 13:19:18 +0900

    feat #16 buisnessLayer 3차시 구현 및 테스트

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import jakarta.transaction.Transactional;
 import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,14 +17,20 @@ import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
+//@Transactional // 이거 되게 좋긴함! 자동으로 clean 시켜주는거 같아서말야!
+// 해당 어노테이션을 넣으면 실제 프로덕션 코드에 @Transactional 이 적용되어있는거 같아 보임! 우리는 안했는데!
+// 되게 뒤늦게 이런 사항이 발견이 될 수 있음! 이런 것들을 쓰지 말자! 라기 보다는 잘 알고 써야 한다는 거임!
+// 롤백 기능때문에 쓰면 너무 좋지만! 부작용에 대해 인지를 하고 사용하는것이 좋지 않을까? 함!
 @ActiveProfiles("test")
 //@DataJpaTest
 class OrderServiceTest {
@@ -36,6 +43,8 @@ class OrderServiceTest {
     private OrderRepository orderRepository;
     @Autowired
     private OrderProductRepository orderProductRepository;
+    @Autowired
+    private StockRepository stockRepository;
 
 
     @AfterEach
@@ -44,6 +53,7 @@ class OrderServiceTest {
         orderProductRepository.deleteAllInBatch();
         productRepository.deleteAllInBatch();
         orderRepository.deleteAllInBatch();
+        stockRepository.deleteAllInBatch();
     }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
@@ -80,6 +90,10 @@ class OrderServiceTest {
 
 
     }
+
+
+
+
 
     @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
     @Test
@@ -122,6 +136,93 @@ class OrderServiceTest {
                 .sellingStatus(ProductSellingStatus.SELLING)
                 .name("메뉴 이름")
                 .build();
+    }
+
+
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock(){
+        //given 테스트 준비 단계는 모두 given
+        LocalDateTime registeredTime = LocalDateTime.now();
+
+
+        Product product1 = createProduct(ProductType.BOTTLE, "001", 1000);
+        Product product2 = createProduct(ProductType.BAKERY, "002", 3000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        //when
+
+        OrderResponse orderResponse = orderService.createOrder(request, registeredTime);
+
+
+        //then
+
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredTime,10000);
+
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("002", 3000),
+                        Tuple.tuple("003", 5000)
+                );
+
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
+
+
+    }
+
+
+
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    @Test
+    void createOrderWithNoStock(){
+        //given 테스트 준비 단계는 모두 given
+        LocalDateTime registeredTime = LocalDateTime.now();
+
+
+        Product product1 = createProduct(ProductType.BOTTLE, "001", 1000);
+        Product product2 = createProduct(ProductType.BAKERY, "002", 3000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+
+        stock1.deductQuantity(1); // todo : 이런식으로 하면 안됨!
+
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        //when //then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
     }
 
 
