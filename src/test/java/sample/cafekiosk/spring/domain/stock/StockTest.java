39656baf66b339150eb8ae5244069e29a1f3744b
Khyojun <nandong5923@naver.com>
2024-11-19 15:42:39 +0900

    feat #28 테스트 간 독립성 보장 학습 및 구현

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -9,6 +9,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StockTest {
 
+    private static final Stock stock = Stock.create("001", 1);
+
+    @DisplayName("재고의 수량이 제공된 수량보다 작은지 확인한다. 공유 자원 사용")
+    @Test
+    void isQuantityLessThanEx(){
+        //given
+        int quantity = 2;
+
+        //when
+        boolean result = stock.isQuantityLessThan(quantity);
+
+        //then
+        assertThat(result).isTrue();
+
+    }
+
+
+    @DisplayName("재고를 주어진 개수만큼 차감할 수 있다. 공유 자원 사용")
+    @Test
+    void deductQuantityEx(){
+        //given
+        int quantity = 1;
+
+
+        //when
+        stock.deductQuantity(quantity);
+
+
+        //then
+        assertThat(stock.getQuantity()).isZero();
+    }
+
+
+
+
     @DisplayName("재고의 수량이 제공된 수량보다 작은지 확인한다.")
     @Test
     void isQuantityLessThan(){
